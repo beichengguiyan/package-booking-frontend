e3fa9cfb737f47325313210bf0b17c54887f41c6
beichengguiyan <chihaotian@zybank.com.cn>
2019-09-05 18:01:14 +0800

    package submit

--- src/store.js
+++ src/store.js
@@ -1,16 +1,65 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from "axios";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    PackageList: [
+      { wayBillNum: "", addressee: "", telephone: "", weight: "" }
+    ]
 
   },
   mutations: {
-
+    initTodos: function (state, todods) {
+      state.PackageList = todods;
+  }
   },
   actions: {
+    updateTodos(context, obj) {
+      const url = "http://localhost:8080/expressDelivery";
+      //put
+      console.log(53, obj)
+      axios
+        .put(url + "/" + obj.id, {
+          content: obj.inputtingItem,
+          status: "active"
+        })
+        .then(function (response) {
+          context.dispath("fetchTodos");
+        })
+        .catch(function (error) {
+          console.log(error.response);
+        });
+    },
 
+    createTodos(context, inputtingItem) {
+      const url = "http://localhost:8080/expressDelivery";
+      //post
+      axios
+        .post(url, { 
+          wayBillNum: "", addressee: "", telephone: "", weight: "" 
+        })
+        .then(function (response) {
+          context.dispath("fetchTodos");
+        })
+        .catch(function (error) {
+          console.log(error.response);
+        });
+    },
+
+    fetchTodos(context) {
+      const url = "http://localhost:8080/expressDelivery";
+      axios
+        .get(url)
+        .then(function (response) {
+          console.log(context);
+          context.commit("initTodos", response.data);
+        })
+        .catch(function (error) {
+          console.log(error, response);
+        });
+    }
   }
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,67 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <a-form :form="form" @submit="handleSubmit">
+
+    <a-form-item label="运单号" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+      <a-input
+        v-decorator="[
+          '运单号',
+          {rules: [{ required: true, message: '请输入运单号!' }]}
+        ]"
+      />
+    </a-form-item>
+
+    <a-form-item label="收件人" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+      <a-input
+        v-decorator="[
+          '收件人',
+          {rules: [{ required: true, message: '请输入收件人!' }]}
+        ]"
+      />
+    </a-form-item>
+
+    <a-form-item label="电话" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+      <a-input
+        v-decorator="[
+          '电话',
+          {rules: [{ required: true, message: '请输入电话!' }]}
+        ]"
+      />
+    </a-form-item>
+
+    <a-form-item label="重量" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+      <a-input
+        v-decorator="[
+          '重量',
+          {rules: [{ required: true, message: '请输入重量!' }]}
+        ]"
+      />
+    </a-form-item>
+
+    <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+      <a-button type="primary" html-type="submit">Submit</a-button>
+    </a-form-item>
+
+  </a-form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      formLayout: "horizontal",
+      form: this.$form.createForm(this)
+    };
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log("Received values of form: ", values);
+          this.$store.dispatch("createTodos",values);
+        }
+      });
+    }
+  }
+};
+</script>
